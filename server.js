2cdefc1b074df8292136e124679ef1f77d6b7c51
Suneet Kumar <Suneet Kumar>
2016-02-04 18:20:26 +0000

    Adding express.Router() functions

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-/*console.log('Method 1');
-
-var http = require('http'),
-fs       = require('fs');
-
-http.createServer(function(req, res){
-	res.writeHead(200, {
-		'Content-Type': 'text/html',
-		'Access-Control-Allow-Origin': '*'
-	});
-	var readStream = fs.createReadStream(__dirname + '/index.html');
-	readStream.pipe(res);	
-}).listen(1337);
-
-console.log('Visit me at http://localhost:1337');*/
 console.log('Method 2');
-var express = require('express');
-var app     = express();
-var path    = require('path');
+var express = require('express'),
+	app     = express(),
+	path    = require('path')
+	adminRouter = express.Router();
+
+adminRouter.use(function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+adminRouter.get('/', function(req, res) {
+	res.send('I am the dashboard!!');
+});
+
+adminRouter.get('/users', function(req, res) {
+	res.send('I show all the users!!');
+});
+
+adminRouter.get('/posts', function(req, res) {
+	res.send('I show all the posts!!');
+});
+
 app.listen(1337);
+app.use('/admin', adminRouter);
+
 console.log('Yuppieee @ http://localhost:1337');
